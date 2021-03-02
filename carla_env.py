87193179b687805addd7f4322ec8361d810980a6
dKolosa <contact@dkolosa.com>
2021-03-01 20:42:27 -0500

    Added framework for reward function .gitignore additions

--- carla_env.py
+++ carla_env.py
@@ -29,6 +29,13 @@ class Carlaenv():
 
         self.vehicle_bp = random.choice(blueprint_library.filter('vehicle.toyota'))
 
+        # The destination will be another spawn point on the map
+        self.destination = random.choice(self.world.get_map().get_spawn_points())
+        self.start_point = random.choice(self.world.get_map().get_spawn_points())
+        self.destination_loc = self.destination.get_forward_vector()
+        self.destination_vec = np.array([self.destination_loc[0], self.destination_loc[1]])
+
+
 
     def setup_sensors(self):
         # setup a dashcam sensor
@@ -47,40 +54,60 @@ class Carlaenv():
         self.actor_list.append(self.sensor_cam)
 
 
-    def step(self):
+    def step(self, action):
         # process action
-        self.vehicle.apply_control(carla.VehicleControl(throttle=random.random(), steer=random.random(), reverse=False))
+        speed, steering_angle, braking = action
+        # Will have to scale/normalize the actions
 
-        # get the image
+        self.vehicle.apply_control(carla.VehicleControl(throttle=speed, steer=steering_angle, braking=braking))
+        self.vehicle.
+        # get the image (next state)
         self.sensor_cam.listen(lambda data: process_image(data))
 
-        # check for collision
-        self.sensor_collision.listen(lambda data: check_collision(data))
-
         # state is the image
-        # reward (distnce)
-        self.calculate_reward()
+        image_state = self.input_image
+        position = self.vehicle.get_location()  # m
+        velocity = self.vehicle.get_velocity()  # m/s
+        acceleration = self.vehicle.get_acceleration() # m/s^2
         
+        pos = np.array([position[0], position[1]])
+        
+        distance = np.sqrt((self.destination_vec[0]-pos[0])**2 + (self.destination_vec[1]-pos[1])**2)
+        # calculate reward
         done = False
-        # check for collision
-        if collision:
-            reward = -10
-        # done (collision??)
+        reward, done = self.reward(distance,action)        
+
+        state = (image_state, pos)
         return state, rewaard, done
 
-    def calculate_reward(self):
-        
-        return rewaard
+    def reward(self, distance, action):
+        '''The reward signal takes the current state of the agent into account.
+        The agent is penalized for bad driving habits (hard braking, high acceleration,
+        sharp turns, driving too close to other vehicles)'''
+
+        speed, steering, brake = action
+        # check for collision
+        self.sensor_collision.listen(lambda data: check_collision(data))
+        if self.collision:
+            collision = 10
+            self.collision = False
+        # reward = destination + speed_limit_threshold - break_force - collisions - change streeing angle -
+                    # jerk + distance_from_cars_threshold
+        reward = -distance - collision
+
+        if 1.0 <= distance <= 3.0:
+            reward = 10
+            done = True
+
+
+        return rewaard, done
 
     def reset(self):
 
         self.actor_list = []
-        # random transform from the list of recommended spawn points of the map.
-        transform = random.choice(self.world.get_map().get_spawn_points())
-
         # So let's tell the world to spawn the vehicle.
-        self.vehicle = self.world.spawn_actor(self.bp, transform)
-
+        self.vehicle = self.world.spawn_actor(self.bp, self.start_point)
+        self.collision = False
         self.setup_sensors()
         self.sensor_cam.listen(lambda data: self.process_image(data))
         
@@ -94,15 +121,14 @@ class Carlaenv():
         self.vehicle.apply_control(self.carla.VehicleControl(throttle=0.0, steer=0.0, reverse=True))
         time.sleep(2)
 
-
-
-    def process_image(data):
+    def process_image(self,data):
         raw = np.array(data)
         self.input_image = np.resshape(raw, (self.img_height, self.img_width, 4))
     # will have to reshape data to an image (w x h x 4)
 
-    def check_collision(data):
+    def check_collision(self,data):
         # check for collision and act accordingly
+        self.collision = True
         print('Collision!!!')
         pass
 
