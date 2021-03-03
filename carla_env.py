e2e728a8309c1d2165e6bfa7119a8423173b44ab
Dan Kolosa <contact@dkolosa.com>
2021-03-02 21:17:52 -0500

    env init working getting segfault after step

--- carla_env.py
+++ carla_env.py
@@ -1,5 +1,7 @@
-import os, sys, glob
+import os, sys, glob, time
 import numpy as np
+import random
+import cv2
 try:
     sys.path.append(glob.glob('../carla/dist/carla-*%d.%d-%s.egg' % (
         sys.version_info.major,
@@ -11,15 +13,16 @@ import carla
 
 class Carlaenv():
 
-    def __init__():
+    def __init__(self):
 
         self.client = carla.Client('localhost', 2000)
         self.client.set_timeout(2.0)
 
         # Image height and width
-        self.img_height = 600
-        self.img_width = 800
+        self.img_height = 64
+        self.img_width = 64
         self.inpuut_image = None
+        self.int_step = 0
 
         # Once we have a client we can retrieve the world that is currently
         self.world = self.client.get_world()
@@ -27,14 +30,13 @@ class Carlaenv():
         # The world contains the list blueprints that we can use for adding new
         blueprint_library = self.world.get_blueprint_library()
 
-        self.vehicle_bp = random.choice(blueprint_library.filter('vehicle.toyota'))
+        self.vehicle_bp = random.choice(blueprint_library.filter('vehicle.*.*'))
 
         # The destination will be another spawn point on the map
         self.destination = random.choice(self.world.get_map().get_spawn_points())
         self.start_point = random.choice(self.world.get_map().get_spawn_points())
-        self.destination_loc = self.destination.get_forward_vector()
-        self.destination_vec = np.array([self.destination_loc[0], self.destination_loc[1]])
-
+        # self.destination_loc = self.destination.get_forward_vector()
+        # self.destination_vec = np.array([self.destination_loc[0], self.destination_loc[1]])
 
 
     def setup_sensors(self):
@@ -42,43 +44,43 @@ class Carlaenv():
         # Find the blueprint of the sensor.
         self.camera_bp = self.world.get_blueprint_library().find('sensor.camera.rgb')
         # Modify the attributes of the blueprint to set image resolution and field of view.
-        self.camera_bp.set_attribute('image_size_x', int(self.img_width))
-        self.camera_bp.set_attribute('image_size_y', int(self.img_height))
+        self.camera_bp.set_attribute('image_size_x', '64')
+        self.camera_bp.set_attribute('image_size_y', '64')
         self.camera_bp.set_attribute('fov', '120')
         # Set the time in seconds between sensor captures
-        self.camera_bp.set_attribute('sensor_tick', '1.0') 
+        self.camera_bp.set_attribute('sensor_tick', '2.0') 
         # move the camera to the dash of the car
         camera_transform = carla.Transform(carla.Location(x=1.5, z=2.4))
 
         self.sensor_cam = self.world.spawn_actor(self.camera_bp, camera_transform, attach_to=self.vehicle)
         self.actor_list.append(self.sensor_cam)
 
-
     def step(self, action):
         # process action
-        speed, steering_angle, braking = action
+        speed = round(float(action[0]),3)
+        steering_angle = round(float(action[1]),3)
+        braking = float(0)
         # Will have to scale/normalize the actions
 
-        self.vehicle.apply_control(carla.VehicleControl(throttle=speed, steer=steering_angle, braking=braking))
-        self.vehicle.
+        self.vehicle.apply_control(carla.VehicleControl(throttle=speed, steer=steering_angle, brake=braking))
+
         # get the image (next state)
         self.sensor_cam.listen(lambda data: process_image(data))
 
         # state is the image
-        image_state = self.input_image
-        position = self.vehicle.get_location()  # m
-        velocity = self.vehicle.get_velocity()  # m/s
-        acceleration = self.vehicle.get_acceleration() # m/s^2
+        # position = self.vehicle.get_location()  # m
+        # velocity = self.vehicle.get_velocity()  # m/s
+        # acceleration = self.vehicle.get_acceleration() # m/s^2
         
-        pos = np.array([position[0], position[1]])
+        # pos = np.array([position[0], position[1]])
         
-        distance = np.sqrt((self.destination_vec[0]-pos[0])**2 + (self.destination_vec[1]-pos[1])**2)
+        # distance = np.sqrt((self.destination_vec[0]-pos[0])**2 + (self.destination_vec[1]-pos[1])**2)
+        distance = 100
         # calculate reward
         done = False
-        reward, done = self.reward(distance,action)        
-
-        state = (image_state, pos)
-        return state, rewaard, done
+        reward = self.reward(distance,action)        
+        print('end step')
+        return self.input_image, reward, done
 
     def reward(self, distance, action):
         '''The reward signal takes the current state of the agent into account.
@@ -93,37 +95,37 @@ class Carlaenv():
             self.collision = False
         # reward = destination + speed_limit_threshold - break_force - collisions - change streeing angle -
                     # jerk + distance_from_cars_threshold
-        reward = -distance - collision
+        reward = -distance
 
         if 1.0 <= distance <= 3.0:
             reward = 10
             done = True
 
 
-        return rewaard, done
+        return reward
 
     def reset(self):
-
         self.actor_list = []
         # So let's tell the world to spawn the vehicle.
-        self.vehicle = self.world.spawn_actor(self.bp, self.start_point)
+        self.vehicle = self.world.spawn_actor(self.vehicle_bp, self.start_point)
         self.collision = False
         self.setup_sensors()
         self.sensor_cam.listen(lambda data: self.process_image(data))
         
+        self.int_step = 0
+
         # setup the collision sensor
         self.collision_bp = self.world.get_blueprint_library().find('sensor.other.collision')
-        collision_transform = self.carla.Transform(self.carla.Location(x=1.5, z=1.0))
+        collision_transform = carla.Transform(carla.Location(x=1.5, z=1.0))
         self.sensor_collision = self.world.spawn_actor(self.collision_bp,collision_transform, attach_to=self.vehicle)
-
         self.actor_list.append(self.sensor_collision)
         self.sensor_collision.listen(lambda data: self.check_collision(data))
-        self.vehicle.apply_control(self.carla.VehicleControl(throttle=0.0, steer=0.0, reverse=True))
+        self.vehicle.apply_control(carla.VehicleControl(throttle=0.0, steer=0.0, reverse=True))
         time.sleep(2)
 
     def process_image(self,data):
-        raw = np.array(data)
-        self.input_image = np.resshape(raw, (self.img_height, self.img_width, 4))
+        raw = np.array(data.raw_data)
+        self.input_image = raw.reshape((self.img_height, self.img_width, 4))
     # will have to reshape data to an image (w x h x 4)
 
     def check_collision(self,data):
@@ -132,13 +134,7 @@ class Carlaenv():
         print('Collision!!!')
         pass
 
-
-    def drive_loop(vehicle):
-        while True:
-            # This is where the action is taken (RL action)
-            vehicle.apply_control(carla.VehicleControl(throttle=random.random(), steer=random.random(), reverse=False))
-            time.sleep(10)
-            vehicle.apply_control(carla.VehicleControl(throttle=random.random(),brake=1.0, reverse=False))
-            time.sleep(10)
-            vehicle.apply_control(carla.VehicleControl(throttle=random.random(), steer=random.random(), reverse=True))
-            time.sleep(10)
+    def show_cam(self):
+        fp_view = self.input_image[:,:,:3]
+        cv2.imshow("",fp_view)
+        cv2.waitKey(1)
--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import sys, os, glob
 import numpy as np
 import cv2
 import time
-import random
-# from carla_env import Carlaenv
+from carla_env import Carlaenv
 
 # Based on the carla tutorial script tutorial.py
 
@@ -16,230 +15,37 @@ except IndexError:
     pass
 import carla
 
+FPS = 15
 
-def process_image(data):
-    raw = np.array(data)
-    # will have to reshape data to an image (w x h x 4)
-
-def check_collision(data):
-    # check for collision and act accordingly
-    print('Collision!!!')
-    pass
-
-
-def drive_loop(vehicle):
-    while True:
-        # This is where the action is taken (RL action)
-        vehicle.apply_control(carla.VehicleControl(throttle=random.random(), steer=random.random(), reverse=False))
-        time.sleep(10)
-        vehicle.apply_control(carla.VehicleControl(throttle=random.random(),brake=1.0, reverse=False))
-        time.sleep(10)
-        vehicle.apply_control(carla.VehicleControl(throttle=random.random(), steer=random.random(), reverse=True))
-        time.sleep(10)
-    
 
 # This sets up the environmnet, have to loop through the environment and update
-actor_list = []
 
-try:
-    # carlaenv = Carlaenv()
-    # requests in the localhost at port 2000.
-    client = carla.Client('localhost', 2000)
-    client.set_timeout(2.0)
+if __name__ == '__main__':
 
-    # Once we have a client we can retrieve the world that is currently
-    world = client.get_world()
+    num_episodes = 10
+    iter_per_episode = 100
 
-    # The world contains the list blueprints that we can use for adding new
-    blueprint_library = world.get_blueprint_library()
-
-    # Now let's filter all the blueprints of type 'vehicle' and choose one
-    # at random.
-    bp = random.choice(blueprint_library.filter('vehicle'))
-
-    # A blueprint contains the list of attributes that define a vehicle's
-    # instance, we can read them and modify some of them. For instance,
-    # let's randomize its color.
-    if bp.has_attribute('color'):
-        color = random.choice(bp.get_attribute('color').recommended_values)
-        bp.set_attribute('color', color)
-
-    # Now we need to give an initial transform to the vehicle. We choose a
-    # random transform from the list of recommended spawn points of the map.
-    transform = random.choice(world.get_map().get_spawn_points())
-
-    # So let's tell the world to spawn the vehicle.
-    vehicle = world.spawn_actor(bp, transform)
-
-    # setup a dashcam sensor
-    # Find the blueprint of the sensor.
-    camera_bp = world.get_blueprint_library().find('sensor.camera.rgb')
-    # Modify the attributes of the blueprint to set image resolution and field of view.
-    camera_bp.set_attribute('image_size_x', '1920')
-    camera_bp.set_attribute('image_size_y', '1080')
-    camera_bp.set_attribute('fov', '110')
-    # Set the time in seconds between sensor captures
-    camera_bp.set_attribute('sensor_tick', '1.0') 
-    # move the camera to the dash of the car
-    camera_transform = carla.Transform(carla.Location(x=1.5, z=2.4))
-    sensor_cam = world.spawn_actor(camera_bp, camera_transform, attach_to=vehicle)
-    actor_list.append(sensor_cam)
+    # init the DL things here
+    carla_env = Carlaenv()
     
-    # setup the collision sensor
-    collision_bp = world.get_blueprint_library().find('sensor.other.collision')
-    sensor_collision = world.spawn_actor(collision_bp,camera_transform, attach_to=vehicle)
+    for i in range(num_episodes):
+        s = carla_env.reset()
+        r = 0
+        done = False
+        j = 0
+        while True:
+        # random action test
+        # carla_env.show_cam()
+            action = np.random.rand(3)
 
-    actor_list.append(sensor_collision)
+            time.sleep(1/FPS)
+            print(j)
+            s1, r, done = carla_env.step(action)
+            j += 1
+            s = s1
+            if done:
+                print(f'Episode over {i} of {num_episodes}')
+                break
 
-    #get the image
-    sensor_cam.listen(lambda data: process_image(data))
-
-    #check for collision
-    sensor_collision.listen(lambda data: check_collision(data))
-
-    # get the velocity as well
-    velocity = vehicle.get_velocity()
-
-    # you can also set the velocity (action)
-    # vehicle.set_velocity()
-
-    # It is important to note that the actors we create won't be destroyed
-    # unless we call their "destroy" function. If we fail to call "destroy"
-    # they will stay in the simulation even after we quit the Python script.
-    # For that reason, we are storing all the actors we create so we can
-    # destroy them afterwards.
-    actor_list.append(vehicle)
-    print('created %s' % vehicle.type_id)
-
-    # Let's put the vehicle to drive around.
-    vehicle.set_autopilot(False)
-
-
-    # But the city now is probably quite empty, let's add a few more
-    # vehicles.
-    transform.location += carla.Location(x=40, y=-3.2)
-    transform.rotation.yaw = -180.0
-    for _ in range(0, 5):
-        transform.location.x += 10.0
-
-        bp = random.choice(blueprint_library.filter('vehicle'))
-
-        # This time we are using try_spawn_actor. If the spot is already
-        # occupied by another object, the function will return None.
-        npc = world.try_spawn_actor(bp, transform)
-        if npc is not None:
-            actor_list.append(npc)
-            npc.set_autopilot(True)
-            print('created %s' % npc.type_id)
-    
-    drive_loop(vehicle)
-
-
-finally:
-    print('destroying actors')
-    # camera.destroy()
-    client.apply_batch([carla.command.DestroyActor(x) for x in actor_list])
-    print('done.')
-
-    # model_dir = os.path.join(os.getcwd(), 'models')
-    # os.makedirs(os.path.join(model_dir, str(datetime.date.today()) + '-' + 'Carla'), exist_ok=True)
-    # save_dir = os.path.join(model_dir, str(datetime.date.today()) + '-' + 'Carla')
-
-    # env = gym.make(ENV)
-    # iter_per_episode = 200
-    # # the state is an input image and current speed (+-1)
-    # n_state = env.observation_space.shape[0]
-    # # the acions are the steering angle and the speed (-1, +1)
-    # n_action = 2
-    # action_bound = 40   # the max mph limit
-
-    # env.seed(1234)
-    # np.random.seed(1234)
-
-    # num_episodes = 1001
-    # PER = True
-
-    # batch_size = 64
-    
-    # # use conv and FC layers to process the images
-
-    # # use FC layers to process the current speed
-    # layer_1_nodes, layer_2_nodes = 512, 512
-
-    # tau = 0.01
-    # actor_lr, critic_lr = 0.0001, 0.001
-    # GAMMA = 0.99
-    # ep = 0.001
-
-    # actor_noise = OrnsteinUhlenbeck(np.zeros(n_action))
-
-    # agent = TDDDPG(n_action, action_bound, layer_1_nodes, layer_2_nodes, actor_lr, critic_lr, PER, GAMMA,tau, batch_size, save_dir)
-
-    # agent.update_target_network(agent.actor, agent.actor_target, agent.tau)
-    # agent.update_target_network(agent.critic, agent.critic_target, agent.tau)
-
-    # load_models = False
-    # save = True
-
-    # # If loading model, a gradient update must be called once before loading weights
-    # if load_models:
-    #     load_model(PER, agent, batch_size, env, ep, n_action, n_state)
-
-    # noise_decay = 1.0
-
-    # for i in range(num_episodes):
-    #     s = env.reset()
-    #     sum_reward = 0
-    #     agent.sum_q = 0
-    #     agent.actor_loss = 0
-    #     agent.critic_loss = 0
-    #     j = 0
-
-    #     while True:
-    #         # env.render()
-
-    #         a = np.clip(agent.actor(tf.convert_to_tensor([s], dtype=tf.float32))[0] + actor_noise()*noise_decay, a_max=action_bound,
-    #                     a_min=-action_bound)
-    #         s1, r, done, _ = env.step(a)
-    #         # Store in replay memory
-    #         if PER:
-    #             error = 1 # D_i = max D
-    #             agent.memory.add(error, (
-    #             np.reshape(s, (n_state,)), np.reshape(a, (n_action,)), r, np.reshape(s1, (n_state,)), done))
-    #         else:
-    #             agent.memory.add(
-    #                 (np.reshape(s, (n_state,)), np.reshape(a, (n_action,)), r, np.reshape(s1, (n_state,)), done))
-    #         agent.train(j)
-
-    #         sum_reward += r
-    #         s = s1
-    #         j += 1
-    #         if done:
-    #             print(f'Episode: {i}, reward: {int(sum_reward)}, q_max: {agent.sum_q / float(j)},\nactor loss:{agent.actor_loss / float(j)}, critic loss:{agent.critic_loss/ float(j)}')
-    #             # rewards.append(sum_reward)
-    #             print('===========')
-    #             if save:
-    #                 agent.save_model()
-    #             if sum_reward > 0:
-    #                 noise_decay = 0.001
-
-    #             break
-
-
-def load_model(PER, agent, batch_size, env, ep, n_action, n_state):
-    for i in range(batch_size + 1):
-        s = env.reset()
-        a = agent.actor(tf.convert_to_tensor([s], dtype=tf.float32))[0]
-        s1, r, done, _ = env.step(a)
-        # Store in replay memory
-        if PER:
-            error = abs(r + ep)  # D_i = max D
-            agent.memory.add(error, (
-                np.reshape(s, (n_state,)), np.reshape(a, (n_action,)), r, np.reshape(s1, (n_state,)), done))
-        else:
-            agent.memory.add(
-                (np.reshape(s, (n_state,)), np.reshape(a, (n_action,)), r, np.reshape(s1, (n_state,)), done))
-    agent.train()
-    agent.load_model()
-
-
+        # for actor in carla_env.actor_list:
+        #     actor.destroy()
