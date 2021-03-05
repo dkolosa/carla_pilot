615b8e64036b1d85667082e537b6b793c94c81a5
Dan Kolosa <contact@dkolosa.com>
2021-03-04 19:56:32 -0500

    removed PER from main

--- main.py
+++ main.py
@@ -2,15 +2,11 @@ import sys, os, glob
 import numpy as np
 import cv2
 import time
-<<<<<<< HEAD
 import datetime
 import random
 from carla_env import Carlaenv
 from TDDDPG import TDDDPG
 from utils import OrnsteinUhlenbeck
-=======
-from carla_env import Carlaenv
->>>>>>> env-dev-rewards
 
 # Based on the carla tutorial script tutorial.py
 
@@ -25,9 +21,9 @@ import carla
 
 FPS = 30
 
-<<<<<<< HEAD
-try:
-    env = Carlaenv()
+if __name__ == '__main__':
+    
+    carla_env = Carlaenv()
 
     model_dir = os.path.join(os.getcwd(), 'models')
     os.makedirs(os.path.join(model_dir, str(datetime.date.today()) + '-' + 'Carla'), exist_ok=True)
@@ -44,7 +40,6 @@ try:
     np.random.seed(1234)
 
     num_episodes = 1001
-    PER = False
 
     batch_size = 1
     
@@ -71,7 +66,7 @@ try:
 
     # If loading model, a gradient update must be called once before loading weights
     # if load_models:
-    #     load_model(PER, agent, batch_size, env, ep, n_action, n_state)
+    #     load_model(agent, batch_size, env, ep, n_action, n_state)
 
     noise_decay = 1.0
 
@@ -81,21 +76,23 @@ try:
     agent.critic_loss = 0
     j = 0
     for i in range(num_episodes):
-        s = env.reset()
+        print('begin episode')
+        s = carla_env.reset()
+        reward = 0
+        done = False
+        j = 0
         while True:
-            # env.render()
+
+            carla_env.show_cam()
 
             a = np.clip(agent.actor(tf.convert_to_tensor([s], dtype=tf.float32))[0] + actor_noise()*noise_decay, a_max=action_bound,
                         a_min=-action_bound)
-            s1, r, done, _ = env.step(a)
+            time.sleep(1/FPS)
+
+            s1, r, done = carla_env.step(a)
             # Store in replay memory
-            if PER:
-                error = 1 # D_i = max D
-                agent.memory.add(error, (
-                np.reshape(s, (n_state,)), np.reshape(a, (n_action,)), r, np.reshape(s1, (n_state,)), done))
-            else:
-                agent.memory.add(
-                    (np.reshape(s, (n_state,)), np.reshape(a, (n_action,)), r, np.reshape(s1, (n_state,)), done))
+            agent.memory.add(
+                (np.reshape(s, (n_state,)), np.reshape(a, (n_action,)), r, np.reshape(s1, (n_state,)), done))
             agent.train(j)
 
             sum_reward += r
@@ -110,58 +107,3 @@ try:
                 if sum_reward > 0:
                     noise_decay = 0.001
                     break  
-
-finally:
-    print('destroy')
-#     env.destroy()
-
-# def load_model(PER, agent, batch_size, env, ep, n_action, n_state):
-#     for i in range(batch_size + 1):
-#         s = env.reset()
-#         a = agent.actor(tf.convert_to_tensor([s], dtype=tf.float32))[0]
-#         s1, r, done, _ = env.step(a)
-#         # Store in replay memory
-#         if PER:
-#             error = abs(r + ep)  # D_i = max D
-#             agent.memory.add(error, (
-#                 np.reshape(s, (n_state,)), np.reshape(a, (n_action,)), r, np.reshape(s1, (n_state,)), done))
-#         else:
-#             agent.memory.add(
-#                 (np.reshape(s, (n_state,)), np.reshape(a, (n_action,)), r, np.reshape(s1, (n_state,)), done))
-#     agent.train()
-#     agent.load_model()
-=======
-
-# This sets up the environmnet, have to loop through the environment and update
-
-if __name__ == '__main__':
-
-    num_episodes = 10
-    iter_per_episode = 100
-
-    # init the DL things here
-    carla_env = Carlaenv()
-    
-    for i in range(num_episodes):
-        print('begin episode')
-        s = carla_env.reset()
-        reward = 0
-        done = False
-        j = 0
-        while True: 
-        # random action test
-            carla_env.show_cam()
-            action = np.random.rand(3)
-
-            time.sleep(1/FPS)
-            s1, reward, done = carla_env.step(action)
-            j += 1
-            s = s1
-            if done:
-                print(f'Episode over {i} of {num_episodes}')
-                break
-
-    
-    # clean up after done
-    carla_env.cleanup()
->>>>>>> env-dev-rewards
